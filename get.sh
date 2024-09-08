6ede721daa92bc2c0379e5a4ab0417a5e735f31b
sebhajek <seb.hajek@gmail.com>
2024-09-08 08:40:13 +0200

    minor fixes

--- get.sh
+++ get.sh
@@ -4,6 +4,7 @@ sudo dnf clean -y all ;
 sudo dnf upgrade -y ;
 sudo dnf install -y curl bash make ;
 rm -rf ~/.dotfiles;
+mkdir -p ~/.dotfiles;
 git clone https://github.com/sebhajek/dotfiles.git ~/.dotfiles ;
 cd ~/.dotfiles ;
 make all -j3 ;
