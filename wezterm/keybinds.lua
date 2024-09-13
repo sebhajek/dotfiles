6f1138d08872a40f1dcc37678ab4d346c201de2c
sebhajek <seb.hajek@gmail.com>
2024-09-13 03:15:26 +0200

    got rid of wezterm keybinds causing collisions with sway

--- wezterm/keybinds.lua
+++ wezterm/keybinds.lua
@@ -1,30 +1,50 @@
 local wezterm = require 'wezterm'
-local defaults = require('defaults')
+local defaults = require 'defaults'
 
 local keybinds = {
-   {
-      key = 'h',
-      mods = 'CTRL|ALT',
-      action = wezterm.action.ActivateTabRelative(-1)
-   },
-   {
-      key = 'l',
-      mods = 'CTRL|ALT',
-      action = wezterm.action.ActivateTabRelative(1)
-   },
-   {
-      key = 'j',
-      mods = 'CTRL|ALT',
-      action = wezterm.action.SpawnCommandInNewTab {
-         domain = 'CurrentPaneDomain',
-         args = defaults.default_shell
-      }
-   },
-   {
-      key = 'k',
-      mods = 'CTRL|ALT',
-      action = wezterm.action.CloseCurrentTab { confirm = true }
-   }
+	{
+		key = 'h',
+		mods = 'CTRL|ALT',
+		action = wezterm.action.ActivateTabRelative(-1),
+	},
+	{
+		key = 'l',
+		mods = 'CTRL|ALT',
+		action = wezterm.action.ActivateTabRelative(1),
+	},
+	{
+		key = 'j',
+		mods = 'CTRL|ALT',
+		action = wezterm.action.SpawnCommandInNewTab {
+			domain = 'CurrentPaneDomain',
+			args = defaults.default_shell,
+		},
+	},
+	{
+		key = 'k',
+		mods = 'CTRL|ALT',
+		action = wezterm.action.CloseCurrentTab { confirm = true },
+	},
+	{
+		key = 'p',
+		mods = 'CTRL|ALT',
+		action = wezterm.action.ActivateCommandPalette,
+	},
+	{
+		key = 'd',
+		mods = 'SHIFT|CTRL|ALT',
+		action = wezterm.action.ShowDebugOverlay,
+	},
+	{
+		key = 'c',
+		mods = 'SHIFT|CTRL',
+		action = wezterm.action.CopyTo 'Clipboard',
+	},
+	{
+		key = 'v',
+		mods = 'SHIFT|CTRL',
+		action = wezterm.action.PasteFrom 'Clipboard',
+	},
 }
 
 return keybinds
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,87 +1,83 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
-local defaults = require('defaults')
+local defaults = require 'defaults'
 
-
-local keybinds = require('keybinds')
+local keybinds = require 'keybinds'
+config.disable_default_key_bindings = true
 config.keys = keybinds
 
 config.enable_wayland = false --gnome wayland cursor fix
 
-
 --[ SHELL SETTINGS]--
 config.default_prog = defaults.default_shell
-
 
 --[ APPEARANCE ]--
 
 config.enable_scroll_bar = false
-config.allow_square_glyphs_to_overflow_width = "Never"
+config.allow_square_glyphs_to_overflow_width = 'Never'
 
-local colors = require('colors')
+local colors = require 'colors'
 config.colors = colors
 
-
-local fonts = require('fonts')
+local fonts = require 'fonts'
 config.font = fonts.term_font
 config.font_size = fonts.term_font_size
 config.freetype_load_target = fonts.antialias.load
 config.freetype_render_target = fonts.antialias.render
-config.foreground_text_hsb = fonts.hsb;
+config.foreground_text_hsb = fonts.hsb
 
 --[ TAB & WINDOW APPEARANCE ]--
 config.window_frame = {
-   font = fonts.gui_font,
-   font_size = fonts.guit_font_size,
-   active_titlebar_bg = colors.background,
-   inactive_titlebar_bg = colors.background,
-   active_titlebar_fg = colors.foreground,
-   inactive_titlebar_fg = colors.ansi[8],
+	font = fonts.gui_font,
+	font_size = fonts.guit_font_size,
+	active_titlebar_bg = colors.background,
+	inactive_titlebar_bg = colors.background,
+	active_titlebar_fg = colors.foreground,
+	inactive_titlebar_fg = colors.ansi[8],
 
-   inactive_titlebar_border_bottom = colors.background,
-   active_titlebar_border_bottom = colors.background,
-   button_fg = colors.ansi[3],
-   button_bg = colors.background,
-   button_hover_fg = colors.ansi[8],
-   button_hover_bg = colors.ansi[1],
+	inactive_titlebar_border_bottom = colors.background,
+	active_titlebar_border_bottom = colors.background,
+	button_fg = colors.ansi[3],
+	button_bg = colors.background,
+	button_hover_fg = colors.ansi[8],
+	button_hover_bg = colors.ansi[1],
 }
 
 config.colors.tab_bar = {
-   background = colors.background,
+	background = colors.background,
 
-   active_tab = {
-      bg_color = colors.background,
-      fg_color = colors.foreground,
-      intensity = 'Normal',
-      underline = 'None'
-   },
+	active_tab = {
+		bg_color = colors.background,
+		fg_color = colors.foreground,
+		intensity = 'Normal',
+		underline = 'None',
+	},
 
-   inactive_tab = {
-      bg_color = colors.background,
-      fg_color = colors.ansi[3],
-      intensity = 'Normal',
-      underline = 'None'
-   },
-   inactive_tab_hover = {
-      bg_color = colors.background,
-      fg_color = colors.ansi[8],
-      intensity = 'Normal',
-      underline = 'None'
-   },
+	inactive_tab = {
+		bg_color = colors.background,
+		fg_color = colors.ansi[3],
+		intensity = 'Normal',
+		underline = 'None',
+	},
+	inactive_tab_hover = {
+		bg_color = colors.background,
+		fg_color = colors.ansi[8],
+		intensity = 'Normal',
+		underline = 'None',
+	},
 
-   new_tab = {
-      bg_color = colors.background,
-      fg_color = colors.ansi[3],
-   },
-   new_tab_hover = {
-      bg_color = colors.background,
-      fg_color = colors.ansi[8],
-      italic = true,
-   },
+	new_tab = {
+		bg_color = colors.background,
+		fg_color = colors.ansi[3],
+	},
+	new_tab_hover = {
+		bg_color = colors.background,
+		fg_color = colors.ansi[8],
+		italic = true,
+	},
 
-   inactive_tab_edge = colors.ansi[1]
+	inactive_tab_edge = colors.ansi[1],
 }
-
 
 return config
