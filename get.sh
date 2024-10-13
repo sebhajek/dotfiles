5415ac869f8ecf0df3ca6442171ea745a748f266
sebhajek <seb.hajek@gmail.com>
2024-10-13 17:49:45 +0200

    changed the get script

--- get.sh
+++ get.sh
@@ -1,10 +1,22 @@
 #!/usr/bin/env bash
 
-sudo dnf clean -y all ;
-sudo dnf upgrade -y ;
-sudo dnf install -y curl bash make ;
-rm -rf ~/.dotfiles;
-mkdir -p ~/.dotfiles;
-git clone https://github.com/sebhajek/dotfiles.git ~/.dotfiles ;
-cd ~/.dotfiles ;
-make all;
+sudo dnf clean -y all
+sudo dnf upgrade -y
+
+sudo dnf install -y curl bash make
+
+mkdir -p ~/.dotfiles
+
+if [ -d "~/.dotfiles/.git" ]; then
+	echo "Repository already exists. Pulling latest changes..."
+	cd ~/.dotfiles
+	git fetch
+	git checkout python-rewrite
+	git pull origin python-rewrite
+else
+	echo "Cloning the repository..."
+	git clone --branch python-rewrite https://github.com/sebhajek/dotfiles.git ~/.dotfiles
+	cd ~/.dotfiles
+fi
+
+make all
