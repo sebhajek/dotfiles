43d733f865a5aee170847141f1075f583d309c03
sebhajek <seb.hajek@gmail.com>
2024-10-14 23:51:17 +0200

    changes

--- languages/js.sh
+++ languages/js.sh
@@ -6,3 +6,27 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
 nvm install --lts
+
+VERSION=$(ls -1 "$HOME/.nvm/versions/node/" | sort -V | tail -n 1)
+NVM_PATH_LINE="export PATH=\"$HOME/.nvm/versions/node/$VERSION/bin:\$PATH\""
+
+if ! grep -q 'export PATH="$HOME/.nvm/versions/node/v[0-9]\+\.[0-9]\+\.[0-9]\+/bin' "$HOME/.profile"; then
+	echo "$NVM_PATH_LINE" >>"$HOME/.profile"
+	echo "Added Node.js version $VERSION to ~/.profile"
+else
+	echo "A Node.js version is already in the PATH in ~/.profile"
+fi
+
+if ! grep -q 'export PATH="$HOME/.nvm/versions/node/v[0-9]\+\.[0-9]\+\.[0-9]\+/bin' "$HOME/.bashrc"; then
+	echo "$NVM_PATH_LINE" >>"$HOME/.profile"
+	echo "Added Node.js version $VERSION to ~/.bashrc"
+else
+	echo "A Node.js version is already in the PATH in ~/.bashrc"
+fi
+
+if ! grep -q 'export PATH="$HOME/.nvm/versions/node/v[0-9]\+\.[0-9]\+\.[0-9]\+/bin' "$HOME/.zshrc"; then
+	echo "$NVM_PATH_LINE" >>"$HOME/.zshrc"
+	echo "Added Node.js version $VERSION to ~/.zshrc"
+else
+	echo "A Node.js version is already in the PATH in ~/.zshrc"
+fi
